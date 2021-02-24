f64120c7f7a4fba85ce0b9f2a8b2ad3be0c2e3b2
st58206 <79593395+st58206@users.noreply.github.com>
2021-02-24 18:47:06 +0100

    firstCommit

---
+++ CV1_Pri01/CV1_Pri01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV1_Pri01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Josef Novák");
+    
+            Console.WriteLine("Jindrišská 16");
+            Console.WriteLine("111 50, Praha 1");
+            Console.ReadKey();
+        }
+    }
+}
