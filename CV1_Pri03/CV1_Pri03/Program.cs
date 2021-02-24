46a28b553c639d5790c9618fba163fb1214ed9e4
st58206 <79593395+st58206@users.noreply.github.com>
2021-02-24 19:52:59 +0100

    CV1 příklad 3

---
+++ CV1_Pri03/CV1_Pri03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV1_Pri03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pohlavi;
+            string rc = "805102/1234";
+            int mes = int.Parse(rc.Substring(2, 2));
+            if ((mes - 50) > 0)
+                pohlavi = "Žena";
+            else
+                pohlavi = "Muž";
+            Console.Write("Pohlaví: ");
+            Console.WriteLine(pohlavi);
+            Console.ReadKey();
+        }
+    }
+}
